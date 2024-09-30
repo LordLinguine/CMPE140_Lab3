bfd6e0ce091e45b1c9872a3afef3b48840832d9b
VanG <126309464+LordLinguine@users.noreply.github.com>
2024-09-30 08:41:36 -0700

    Initial commit

---
+++ project_1.srcs/sources_1/new/cpu.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2024 01:44:29 PM
+// Design Name: 
+// Module Name: cpu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cpu( input logic rst_n, clk,
+            input logic [31:0] imem_insn, dmem_data,
+            output logic [31:0] imem_addr, dmem_addr,
+            output logic dmem_wen
+
+    );
+    
+    
+    
+    
+endmodule
